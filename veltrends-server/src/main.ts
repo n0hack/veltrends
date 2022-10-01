60ae3912624135c1a8002e4f1f4b9713ab7d7075
n0hack <nohack-@naver.com>
2022-10-02 05:31:34 +0900

    [feat] fastify-swagger 설정

--- veltrends-server/src/main.ts
+++ veltrends-server/src/main.ts
@@ -1,10 +1,14 @@
 import Fastify from 'fastify';
+import fastifySwagger from '@fastify/swagger';
+import routes from './routes/index.js';
+import { swaggerConfig } from './config/swagger.js';
 
-const server = Fastify({});
-
-server.get('/ping', async (request, reply) => {
-  return 'pong\n';
+const server = Fastify({
+  logger: true,
 });
+
+await server.register(fastifySwagger, swaggerConfig);
+server.register(routes);
 
 server.listen({ port: 8000 }, (err, address) => {
   if (err) {
