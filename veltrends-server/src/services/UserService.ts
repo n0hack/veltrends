367f0ffda8ba88837e21cb1909a30f7541069b9b
n0hack <nohack-@naver.com>
2022-10-03 00:02:39 +0900

    [feat] Prisma 통합 & 회원가입 일부 구현

--- veltrends-server/src/services/UserService.ts
+++ veltrends-server/src/services/UserService.ts
@@ -1,3 +1,13 @@
+import bcrypt from 'bcrypt';
+import db from '../lib/db.js';
+
+const SALT_ROUNDS = 10;
+
+interface AuthParams {
+  username: string;
+  password: string;
+}
+
 class UserService {
   private static instance: UserService;
 
@@ -8,8 +18,20 @@ class UserService {
     return UserService.instance;
   }
 
-  register() {
-    return 'registered!';
+  async register({ username, password }: AuthParams) {
+    const exists = await db.user.findUnique({ where: { username } });
+    if (exists) {
+      throw new Error('User already exists');
+    }
+
+    const hash = await bcrypt.hash(password, SALT_ROUNDS);
+    const user = await db.user.create({
+      data: {
+        username,
+        passwordHash: hash,
+      },
+    });
+    return user;
   }
 
   login() {
